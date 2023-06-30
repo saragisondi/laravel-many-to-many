72973af9695cfb7162ec32e0fe87df823a26b536
Sara Gisondi <sara.gisondi@outlook.it>
2023-06-30 12:04:02 +0200

    modify dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="py-5 mx-5 container-fluid ">
+<div class="py-5 mx-5 overflow-auto container-fluid">
   <h2 class="my-4 fs-4 text-secondary fw-bold">
       Home Dashboard
   </h2>
@@ -9,15 +9,18 @@
   <h4 class="fw-bold">Numero Progetti: {{ $n_project }} </h4>
   <a href="{{route('admin.projects.create')}}" class="mx-2 my-3 btn btn-primary fw-bold">Nuovo Progetto</a>
 
-  <div class="row justify-content-center">
+  <div class="row d-flex justify-content-center">
 
-    <h4 class="my-5">Ultimo Progetto: <span class="fw-bold">{{ $last_project->title }}</span> </h4>
+    <h4 class="my-5 d-flex justify-content-center">Ultimo Progetto:
+      <h5 class="text-center text-uppercase fs-2 fw-bold"><a class=" text-decoration-none text-reset fw-bold" href="{{route('admin.projects.show', $last_project)}}">{{ $last_project->title }}</a>
+      </h5>
+    </h4>
 
-    <div>
-      <h5 class="fw-bold">{{ $last_project->title }}</h5>
-      <img class="w-50" src="{{asset('storage/' . $project->image_path)}}" alt="{{$project->title}}">
-      <p>{!! $last_project->text !!}</p>
+
+    <div class="d-flex justify-content-center">
+      <img class="w-50" src="{{asset('storage/' . $project->image_path)}}" onerror="this.src='https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg'">
     </div>
+    <p>{!! $last_project->text !!}</p>
 
   </div>
 </div>
